78ae5e68d3b633e57ccc50043ae9fd0dda9025d0
Bastiaan Welmers <bwelmers@cloudsuite.com>
2019-03-20 15:36:46 +0100

    Add doc

--- backup.sh
+++ backup.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+# Script to synchronize homedir to external host, encrypted by 
+# encfs --reverse.
+# With extra feature to exclude directories that are not relevant
+# to make backup of.
+# 
+# REQUIRES:
+# - a setup encfs --reverse from $HOME. This wil require a $HOME/.encfs6.xml
+# - the password that is used for the encryption to be stored in ~/.encfs_pwd
+#
+# To meet these requirements; do:
+#
+# $ mkdir /var/tmp/$USER-export
+# $ pwgen -s 16 1 > ~/.encfs_pwd
+# $ encfs --reverse $HOME /var/tmp/$USER-export
+# and then use the standard options, and fill in the password 
+# from ~/.encfs_pwd
+# 
+# next test it with:
+# $ fusermount -u /var/tmp/$USER-export
+# $ encfs --reverse --extpass="/bin/cat $HOME/.encfs_pwd" $HOME /var/tmp/$USER-export
+# 
+# Done!
+
 EXCLUDES=`cat <<EOF
 snap
 nobackup
