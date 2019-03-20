a472f74b09712ce33d3d4e66194279393e84d699
Bastiaan Welmers <bwelmers@cloudsuite.com>
2019-03-20 15:46:44 +0100

    Remove stub code

--- backup.sh
+++ backup.sh
@@ -63,11 +63,3 @@ fusermount -u $mountpoint
 today=`date +%Y_%m_%d`
 ssh $EXTERNAL_SSH cp -al storage/t450s storage/snapshots/t450s/$today
 
-#rsync -rlptvx --delete --exclude tmp --exclude .cache --exclude build \
-#  --exclude snap --exclude .thunderbird --exclude nobackup \
-#  --exclude .WebIde70/system --exclude .crypt-baardmans \
-#  --exclude VirtualBox\ VMs --exclude Seafile \
-#  /home/bastiaan/ nas:/vol/storage/t450s/home/bastiaan
-#TODAY=`date +%Y_%m_%d`
-## maak snapshot van backup
-#ssh nas cp -al /vol/storage/t450s /vol/storage/snapshots/t450s/$TODAY
