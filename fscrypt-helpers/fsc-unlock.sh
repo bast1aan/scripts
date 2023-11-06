b3d0d85c0288c9da81bafb13e1eda814404f528b
Bastiaan Welmers <bastiaan@welmers.net>
2023-11-06 21:34:57 +0100

    Add systemd unit for prompting during boot

--- fscrypt-helpers/fsc-unlock.sh
+++ fscrypt-helpers/fsc-unlock.sh
@@ -15,9 +15,11 @@ conf_file=$(dirname ${BASH_SOURCE[0]})/../etc/fsc/$user.sh
 
 . $conf_file
 
-echo -n "Password: " 1>&2
-read -s FSC_PASSWORD
-echo 1>&2
+if [ -z "$FSC_PASSWORD" ]; then
+	echo -n "Password: " 1>&2
+	read -s FSC_PASSWORD
+	echo 1>&2
+fi
 
 for dir in $fsc_dirs; do
 	if [ -d $dir ]; then
