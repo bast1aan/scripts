4d08c25f7097c57e4a05a50694baee44f2a178c8
Bastiaan Welmers <bastiaan@welmers.net>
2024-10-18 22:05:00 +0000

    Script for converting phone media to lower quality

---
+++ lq-jpg-mp4.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+for i in *jpg; do echo $i; convert -resize 50% -quality 83% $i lq/$i; done
+for i in *mp4; do ffmpeg -i $i -acodec copy -vcodec libx264 -preset slower -crf 25 -threads 0 -vf scale="iw/1.5:ih/1.5" lq/$i ; done
