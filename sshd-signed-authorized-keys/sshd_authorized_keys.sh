b8a72a08cb10ed5364761b6ecbef7bce4984728d
Bastiaan Welmers <bastiaan@welmers.net>
2025-02-01 22:16:57 +0100

    Access files in user homedir as user
    Some environments don't give access root to homedir users, like
     - homedirs living in fuse mounted fs'es like encfs
     - homedirs mounted from NFS with root squash on

--- sshd-signed-authorized-keys/sshd_authorized_keys.sh
+++ sshd-signed-authorized-keys/sshd_authorized_keys.sh
@@ -5,14 +5,21 @@ home=$2
 
 user_authorized_keys="$home/.ssh/authorized_keys"
 signed_authorized_keys="/var/local/sshd_signed_authorized_keys/$user"
+uid=$(id -u $user)
 
+as_user() {
+	setpriv --reuid $uid "$@"
+}
 
-if [ -f "$user_authorized_keys.sig" ]; then
+if as_user [ -f "$user_authorized_keys.sig" ]; then
+	keyrings=''
 	for f in /usr/local/etc/sshd_authorized_keys_keyring/*; do
 		keyrings="$keyrings --keyring=$f "
 	done
-	if gpgv $keyrings $user_authorized_keys.sig $user_authorized_keys 2>/dev/null >/dev/null; then
-		cp $user_authorized_keys $signed_authorized_keys
+	if as_user gpgv $keyrings $user_authorized_keys.sig $user_authorized_keys 2>/dev/null >/dev/null; then
+		tmpf=$(as_user mktemp)
+		as_user cp $user_authorized_keys $tmpf
+		mv $tmpf $signed_authorized_keys
 		chown root:root $signed_authorized_keys
 		chmod 400 $signed_authorized_keys
 		setfacl -m u:$user:r $signed_authorized_keys
@@ -23,10 +30,10 @@ fi
 if [ -f $signed_authorized_keys ]; then
 	cat $signed_authorized_keys
 else
-	if [ -f $user_authorized_keys ]; then
-		cat $user_authorized_keys
+	if as_user [ -f $user_authorized_keys ]; then
+		as_user cat $user_authorized_keys
 	fi
-	if [ -f ${user_authorized_keys}2 ]; then
-		cat ${user_authorized_keys}2
+	if as_user [ -f ${user_authorized_keys}2 ]; then
+		as_user cat ${user_authorized_keys}2
 	fi
 fi
