f7fecbd20a5e5eb932248c559aa93ba1974098a7
Bastiaan Welmers <bastiaan@welmers.net>
2025-02-06 00:06:50 +0100

    Make mtime stamps match from originals

--- lq-jpg-mp4.sh
+++ lq-jpg-mp4.sh
@@ -2,3 +2,5 @@
 
 for i in *jpg; do echo $i; convert -resize 50% -quality 83% $i lq/$i; done
 for i in *mp4; do ffmpeg -i $i -acodec copy -vcodec libx264 -preset slower -crf 25 -threads 0 -vf scale="iw/1.5:ih/1.5" lq/$i ; done
+for i in *jpg; do touch -r $i lq/$i; done
+for i in *mp4; do touch -r $i lq/$i; done
